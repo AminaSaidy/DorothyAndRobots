9ca56fcc861ff8056bd11c37950a1b592d8e89f8
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-07-31 20:55:47 +0500

    boolean containsNonsenseChars - recievce a word to check if there are other characters than a-z or A-Z

--- src/main/java/uzb/aminasaidakhmedova/dorothyandrobots/model/_Main.java
+++ src/main/java/uzb/aminasaidakhmedova/dorothyandrobots/model/_Main.java
@@ -4,4 +4,9 @@ public class _Main {
     public static void main (String[] args) {
 
     }
+
+    private static boolean containsNonsenseChars(String word) {
+        return word.matches(".*[^a-zA-Z].*]");
+    }
 }
+
