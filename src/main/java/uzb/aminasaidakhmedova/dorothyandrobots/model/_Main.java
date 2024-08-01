bd2e6301a8cff3ba2d59d53e5911e643ce56fe3e
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-08-01 18:51:04 +0500

    createAndWriteToFile function modified

--- src/main/java/uzb/aminasaidakhmedova/dorothyandrobots/model/_Main.java
+++ src/main/java/uzb/aminasaidakhmedova/dorothyandrobots/model/_Main.java
@@ -4,13 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class _Main {
     public static void main(String[] args) {
         String resourcesRode = "src/main/resources/";
         String inputFile = resourcesRode + "text.txt";
-        String firstErrorFile = null;
-        String secondErrorFile = null;
+        String firstErrorWord = null;
+        String secondErrorWord = null;
+        Set<String> evenVowelsWords = new HashSet<>();
+        Set<String> oddVowelsWords = new HashSet<>();
 
         try (FileReader fr = new FileReader(inputFile);
              BufferedReader br = new BufferedReader(fr)) {
@@ -24,33 +28,36 @@ public class _Main {
                     if (containsNonsenseChars(word)) {
                         errorWordCount++;
                         if (errorWordCount == 1) {
-                            firstErrorFile = resourcesRode + deleteErrFromWord(word) + ".txt";
-                            createFile(firstErrorFile);
+//                            firstErrorFile = resourcesRode + deleteErrFromWord(word) + ".txt";
+                            firstErrorWord = word;
                         } else if (errorWordCount == 2) {
-                            secondErrorFile = resourcesRode + deleteErrFromWord(word) + ".txt";
-                            createFile(secondErrorFile);
-                            break;
+//                            secondErrorFile = resourcesRode + deleteErrFromWord(word) + ".txt";
+                            secondErrorWord = word;
                         }
-                    }
-                    if (errorWordCount == 2) {
-                        break;
+                    } else {
+                        if (countVowels(word) % 2 == 0){
+                            evenVowelsWords.add(word);
+                        } else {
+                            oddVowelsWords.add(word);
+                        }
                     }
                 }
             }
-            System.out.println("Files 1 and 2 were created.");
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-
-        createFile("punctuation");
+        createAndWriteToFile (resourcesRode + firstErrorWord + ".txt", evenVowelsWords.toString());
+        createAndWritetoFIle(resourcesRode + secondErrorWord + ".txt", oddVowelsWords.toString());
+        createAndWriteToFile("punctuation");
     }
 
     public static boolean containsNonsenseChars(String word) {
         return word.matches(".*[^a-zA-Z].*]");
     }
 
-    public static void createFile(String fileName) {
+    public static void createAndWriteToFile(String fileName, String content) {
         try (FileWriter fw = new FileWriter(fileName)) {
+            fw.write(content);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
@@ -69,5 +76,7 @@ public class _Main {
         }
         return count;
     }
+
+
 }
 
